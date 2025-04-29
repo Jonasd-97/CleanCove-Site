c2b6913390045f697ef843fa1d4d04f21e98936a
Jonasd-97 <jonasdlaboo@gmail.com>
2025-04-28 20:50:12 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,6 @@
+// Firebase is temporarily disabled for CleanCove deployment.
+// This placeholder avoids build errors until Firebase setup is ready.
+
+export default function firebaseApp() {
+  return null;
+}
