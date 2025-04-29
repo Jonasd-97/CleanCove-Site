3dae4ade7b496facfa8dc164f92d0c2910a5a478
Jonasd-97 <jonasdlaboo@gmail.com>
2025-04-28 20:48:51 -0400

    Create HomePage.jsx

---
+++ src/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to CleanCove</h1>
+      <p>Eco-friendly cleaning solutions powered by bamboo.</p>
+    </div>
+  );
+}
+
+export default HomePage;
